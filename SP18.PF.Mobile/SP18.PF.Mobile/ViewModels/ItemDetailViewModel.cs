dc20aef326920828ebb577a7405d3fc71184a0da
Cole Westenhiser <cole.westenhiser@selu.edu>
2018-04-10 13:11:02 -0500

    Added Xamarin Forms to Project

---
+++ SP18.PF.Mobile/SP18.PF.Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SP18.PF.Mobile.Models;
+
+namespace SP18.PF.Mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
