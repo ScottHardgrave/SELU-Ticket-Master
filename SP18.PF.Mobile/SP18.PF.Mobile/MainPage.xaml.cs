bbf7c6763338ed80de10ecf6295bd51ae2f1f276
Cole Westenhiser <cole.westenhiser@selu.edu>
2018-04-16 23:37:19 -0500

    Using allTickets api I made for now until login API is used. Have first object in the array displaying when you press view my tickets. Baby steps. More completed work on connection string. Had to add references. Not sure if you have to, but you may need to install nuget package "Microsoft.CSharp"

--- SP18.PF.Mobile/SP18.PF.Mobile/MainPage.xaml.cs
+++ SP18.PF.Mobile/SP18.PF.Mobile/MainPage.xaml.cs
@@ -1,18 +1,31 @@
-﻿using System;
+﻿using SP18.PF.Mobile.Models;
+using SP18.PF.Mobile.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace SP18.PF.Mobile
 {
-	public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
 	{
 		public MainPage()
 		{
 			InitializeComponent();
+
+            BindingContext = new TicketDto();
 		}
+
+        private async void GetTicketBtn_Clicked(object sender, EventArgs e)
+        {
+            
+            TicketDto ticket = await ConnectionString.GetTicket();
+            BindingContext = ticket;
+        }
 
         protected override bool OnBackButtonPressed()
         {
